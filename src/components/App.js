2d4a057034fb1affe9beef20cf58bdc18c0cca76
elias-cardon <62133614+elias-cardon@users.noreply.github.com>
2020-12-21 15:54:57 +0100

    MàJ

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import Nav from './Nav'
-import SearchArea from "./SearchArea";
-import MovieList from "./MovieList";
+import SearchArea from './SearchArea'
+import MovieList from './MovieList'
+import Pagination from './Pagination'
 
 class App extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             movies: [],
             searchTerm: '',
+            totalResult: 0,
+            currentPage: 1,
+            currentMovie: null,
             apiKey:
             process.env.REACT_APP_API
         }
@@ -17,11 +21,11 @@ class App extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.state.apiKey}&query=${this.state.searchTerm} `)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm} `)
             .then(data => data.json())
             .then(data => {
                 console.log(data)
-                this.setState({movies: [...data.results]})
+                this.setState({movies: [...data.results], totalResults: data.total_results})
             })
     }
 
@@ -29,12 +33,24 @@ class App extends Component {
         this.setState({searchTerm: e.target.value})
     }
 
+    nextPage = (pageNumber) =>
+    {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}&page=${pageNumber}`)
+            .then(data => data.json())
+            .then(data => {
+                console.log(data)
+                this.setState({movies: [...data.results], currentPage : pageNumber})
+            })
+    }
+
     render() {
+        const numberPages = Math.floor(this.state.totalResult / 20);
         return (
             <div className="App">
                 <Nav/>
                 <SearchArea handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
                 <MovieList movies={this.state.movies}/>
+                {this.state.totalResult > 20 ? <Pagination pages={numberPages} nextPage={this.nextPage()} currentPage={this.state.currentPage}/> : ''}
             </div>
         );
     }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,13 +5,9 @@ const Movie = (props) => {
         <div className="col s12 m6 l3">
             <div className="card">
                 <div className="card-image waves-effect waves-block waves-light">
-                    {
-                        props.image == null ?
-                            <img src={'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'}
-                                 alt="card image" style={{width: "100%", height: 360}}/> :
-                            <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt="card image"
-                                 style={{width: "100%", height: 360}}/>
-                                 }
+                    { props.image == null ? <img className="" src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt="Card image cap" style={{ width: "100%",
+                        height: 360}}/> : <img className="" src={`http://image.tmdb.org/t/p/w185${props.image}`} alt="Card image cap" style={{ width: "100%",
+                        height: 360}} />}
                     </div>
                 <div className="card-content">
                     <p><a href="#">Voir les détails du film</a></p>
